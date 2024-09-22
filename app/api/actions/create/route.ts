1a2d9ed58e6151fe04cdda82d420f54693dd5d44
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2024-09-22 19:47:34 +0530

    fees changed

--- app/api/actions/create/route.ts
+++ app/api/actions/create/route.ts
@@ -5,7 +5,6 @@ import {
   SystemProgram,
   Connection,
   clusterApiUrl,
-  LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
 import {
   ACTIONS_CORS_HEADERS,
@@ -73,7 +72,7 @@ export const POST = async (req: NextRequest) => {
       SystemProgram.transfer({
         fromPubkey: userPubKey,
         toPubkey: new PublicKey(MY_PUB_KEY),
-        lamports: 0.003 * LAMPORTS_PER_SOL, 
+        lamports: 0, 
       })
     );
 
